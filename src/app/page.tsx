0c439c029671c8a33e44b07982cf3fc9baa8fedb
Kaushik Nag Tumu <138563218+404kaushik@users.noreply.github.com>
2024-11-28 20:42:19 -0500

    Resolving even more deployment issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 'use client'
-import { useState } from 'react';
 import ChatInterface from '@/components/ChatInterface';
-import type { AppProps } from 'next/app'
 import { GroqProvider } from '@/context/GroqContext';
 
 export default function Home() {
